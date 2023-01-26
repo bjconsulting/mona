51f040a23c0dd6f283570471d9aba5208b553560
Fabrício Bahiense Santos <51688346+FabbSantos@users.noreply.github.com>
2023-01-26 19:37:25 -0300

    all major best practices errors fixed (#4)
    * first and second sections done
    
    * table done
    
    * final mobile adjusts
    
    * all done
    
    * correcting spacing.
    
    * scripts and favicon added
    
    * best practices errors fixed
    
    * all fixed

--- consorcio_noma/src/components/router-head/router-head.tsx
+++ consorcio_noma/src/components/router-head/router-head.tsx
@@ -6,7 +6,9 @@ import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
  */
 export const RouterHead = component$(() => {
   const clarity = `(function(c,l,a,r,i,t,y){
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }};
+
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }
+
   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", "f635ucsahn");`
