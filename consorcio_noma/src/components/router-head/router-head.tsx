5fd724561e63ccb37dac2c63d95f949fce12329b
Fabrício Bahiense Santos <51688346+FabbSantos@users.noreply.github.com>
2023-01-26 19:06:35 -0300

    Added clarity and pixel scripts and favicon  (#3)
    * first and second sections done
    
    * table done
    
    * final mobile adjusts
    
    * all done
    
    * correcting spacing.
    
    * scripts and favicon added

--- consorcio_noma/src/components/router-head/router-head.tsx
+++ consorcio_noma/src/components/router-head/router-head.tsx
@@ -5,6 +5,29 @@ import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
+  const clarity = `(function(c,l,a,r,i,t,y){
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "f635ucsahn");`
+
+  // < !--Meta Pixel Code -->
+  const pixel = ` !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '510661534225018');
+    fbq('track', 'PageView');`
+
+  const noscript = `<img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=510661534225018&ev=PageView&noscript=1"
+    />`
+
+
   const head = useDocumentHead();
   const loc = useLocation();
 
@@ -14,7 +37,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image" href="/icon.png" />
 
       {head.meta.map((m) => (
         <meta {...m} />
@@ -27,6 +50,10 @@ export const RouterHead = component$(() => {
       {head.styles.map((s) => (
         <style {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
+
+      <script > {clarity} </script>
+      <script> {pixel} </script>
+      <noscript>{noscript}</noscript>
     </>
   );
 });
